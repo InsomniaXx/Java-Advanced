2b2dc5969baf2b6dd0955ce495c6d8b04d15277c
Elzhana Bilkovska <89201832+InsomniaXx@users.noreply.github.com>
2021-11-28 14:03:59 +0200

    Delete StreamsFilesDirectories directory

--- StreamsFilesDirectories/L08NestedFolders.java
+++
@@ -1,7 +0,0 @@
-package Advanced.StreamsFilesDirectories;
-
-public class L08NestedFolders {
-    public static void main(String[] args) {
-
-    }
-}
--- StreamsFilesDirectories/L09SerializeCustomObject.java
+++
@@ -1,7 +0,0 @@
-package Advanced.StreamsFilesDirectories;
-
-public class L09SerializeCustomObject {
-    public static void main(String[] args) {
-
-    }
-}
